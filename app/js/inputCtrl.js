2ec9a2c552312b286a04273bf212359b54ee0bb4
Frida <friekl@kth.se>
2016-03-31 16:14:07 +0200

    test-css

--- app/js/inputCtrl.js
+++ app/js/inputCtrl.js
@@ -2,13 +2,20 @@ StarWarsApp.controller('inputCtrl', function($scope, StarModel){
 
 $scope.inputName=function(query){
 	StarModel.addToProfile('name', query)
-	}
+}
 
 
-$scope.submitHairEyeHeight=function(hair, eye, height){
+$scope.inputHair=function(hair){
 	StarModel.addToProfile('hair', hair)
+}
+
+$scope.inputEye=function(eye){
 	StarModel.addToProfile('eye', eye)
+}
+
+$scope.inputHeight=function(height){
 	StarModel.addToProfile('height', height)
-	}
+}
+
 
 });
