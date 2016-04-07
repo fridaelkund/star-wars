0648945021d8d20b971e2692ce3a7a65adeda818
Frida <friekl@kth.se>
2016-04-07 15:44:06 +0200

    Fint glatt etc

--- app/js/inputCtrl.js
+++ app/js/inputCtrl.js
@@ -1,5 +1,11 @@
 StarWarsApp.controller('inputCtrl', function($scope, StarModel){
 
+$scope.currentheight = "100";
+$scope.currentfeet = "280";
+	
+console.log($scope.currentfeet);
+console.log($scope.currentheight);
+
 $scope.inputName=function(query){
 	if(query==null){
 		alert("Please fill in your name!");
@@ -10,25 +16,28 @@ $scope.inputName=function(query){
 }
 }
 
-
-$scope.inputHair=function(hair){
-	StarModel.addToProfile('hair', hair)
+$scope.inputHair=function(color, hex){
+	StarModel.addToProfile('hair', color)
+	$scope.currenthair = hex;
 }
 
-$scope.inputEye=function(eye){
+$scope.inputEye=function(eye, hex){
 	StarModel.addToProfile('eye', eye)
+	$scope.currenteye = hex;
 }
 
 $scope.inputHeight=function(height){
 	StarModel.addToProfile('height', height)
+	$scope.currentfeet = String(height+180);
+	$scope.currentheight = String(height);
+	console.log($scope.currentfeet);
+	console.log($scope.currentheight);
 }
 
 $scope.profil = StarModel.returnProfile();
    
 $scope.eyecols=StarModel.eyecol;
 $scope.haircols = StarModel.haircol;
-
-console.log($scope.haircols["blue"]);
 
 });
 
