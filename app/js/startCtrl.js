4516385931fe9781a94061e8da6d9edefadf0db6
Frida <friekl@kth.se>
2016-04-20 21:42:39 +0200

    Ny startview + meny :)

--- app/js/startCtrl.js
+++ app/js/startCtrl.js
@@ -1,25 +1,25 @@
 StarWarsApp.controller('startCtrl', function($scope, StarModel){
 
-$scope.data = [];
-var planetUrl = "http://swapi.co/api/planets/"
-var peopleUrl = "http://swapi.co/api/people/"
+//$scope.data = [];
+//var planetUrl = "http://swapi.co/api/planets/"
+//var peopleUrl = "http://swapi.co/api/people/"
 
 
-$scope.fetchPlanets = function(){
-	console.log("I fetch planets")
-	StarModel.fetchAll(planetUrl).then(function(response){
-		$scope.planets = response;
-		StarModel.addPlanets($scope.planets);	
-		$scope.fetchPeople();
-	})
-};
+//$scope.fetchPlanets = function(){
+//	console.log("I fetch planets")
+// 	StarModel.fetchAll(planetUrl).then(function(response){
+// 		$scope.planets = response;
+// 		StarModel.addPlanets($scope.planets);	
+// 		$scope.fetchPeople();
+// 	})
+// };
 
-$scope.fetchPeople = function(){
-	console.log("I fetch people")
-	StarModel.fetchAll(peopleUrl).then(function(response){
-		$scope.people = response;
-		StarModel.addPeople($scope.people);	
-	})
-};
+// $scope.fetchPeople = function(){
+// 	console.log("I fetch people")
+// 	StarModel.fetchAll(peopleUrl).then(function(response){
+// 		$scope.people = response;
+// 		StarModel.addPeople($scope.people);	
+// 	})
+// };
 
 });
