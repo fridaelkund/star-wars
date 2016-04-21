237c01b4276ada6956bc94bbbfee66656a9b0f3a
runhem <alexandra@ep.se>
2016-04-21 10:37:04 +0200

    Alex commit

--- app/js/startCtrl.js
+++ app/js/startCtrl.js
@@ -20,14 +20,23 @@ $scope.fetchPeople = function(){
 	StarModel.fetchAll(peopleUrl).then(function(response){
 		$scope.people = response;
 		StarModel.addPeople($scope.people);
-		return $scope.people 
 	})
 };
 
 $scope.loading = function(){
-$scope.fetchPlanets();
-
+	
+	$scope.fetchPlanets();
 };
+
+$scope.play = function(startwars){
+	$scope.show="show";
+	document.getElementById('audio1').play(startwars);
+	TweenMax.from(".test",1, {y:200, x:100});
+	TweenMax.from(".menu", 1, {opacity:0});
+	TweenMax.to(".test",10, {y:0, x:100});
+	TweenMax.to(".menu", 10, {opacity:1});
+
+}
 
 });
 
