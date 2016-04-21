177f2f95833a1e19b4b4929776ce787a008a61c3
josmol <josmol@kth.se>
2016-04-21 19:18:35 +0200

    profile

--- app/js/profileCtrl.js
+++ app/js/profileCtrl.js
@@ -3,7 +3,10 @@ StarWarsApp.controller('profileCtrl', function($scope, StarModel){
 $scope.profil = StarModel.returnProfile();
 
 $scope.wonplanets = StarModel.returnWonPlanets();
+console.log("wonp", $scope.wonplanets);
 $scope.lostplanets = StarModel.returnLostPlanets();
+console.log("LOSTp", $scope.lostplanets);
+
 
 $scope.amountOfWonPlanets = $scope.wonplanets.length;
 $scope.amountOfLostPlanets = $scope.lostplanets.length;
