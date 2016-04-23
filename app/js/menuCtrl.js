6da1540e27629fab2b482ad81a6cbd6e224ef990
Frida <friekl@kth.se>
2016-04-23 13:56:48 +0200

    menyn funkar halleluja

--- app/js/menuCtrl.js
+++ app/js/menuCtrl.js
@@ -8,31 +8,29 @@ var peopleUrl = "http://swapi.co/api/people/"
 // checking if user has previous api-response in local storage.
 // If so, fetching the data from local storage. If not, calling the API and fetching 
 // people and planets from API. 
+
 $scope.loading = function(startwars){	
-	// While waiting we're setting loading-variable to true and hiding play-button
 	$scope.loading = true;
+
+	// While waiting we're setting loading-variable to true and hiding play-button
 	// If no planets or people in local storage, we have to get them from API 
 	if(StarModel.checkLocalStorage() === false){
 		$scope.fetchPlanets();
-	
-		$timeout(function() {
-    		$scope.loadingmessage = 'First time here? Fetching data';	
-   	 }, 2000);
+		
+		var t2 = new TimelineMax();
+		t2.to('#loading-message', 0.001, {text:{value:"Loading data..."}, delay:3});
+		t2.to('#loading-message', 0.001, {text:{value:"Getting the force..."}, delay:3});
+		t2.to('#loading-message', 0.001, {text:{value:"Sharpen laser swords..."}, delay:3});
+		t2.to('#loading-message', 0.001, {text:{value:"Filling up spaceships..."}, delay:3});
 
-		$timeout(function() {
-    		$scope.loadingmessage = 'Tankar rymdskepp...';	
-   	 }, 3000);
+		// Playing Star Wars-intro while loading to get into the right mode. 
+		document.getElementById('audio1').play(startwars);
 
-		$timeout(function() {
-    		$scope.loadingmessage = 'Kammar wookies...';	
-   	 }, 4000);
-
-	// Playing Star Wars-intro while loading to get into the right mode. 
-	document.getElementById('audio1').play(startwars);
 	}else{
 
 // If we have planets and people in local storage, get them! 
 		$scope.habitantsOnPlanets = StarModel.getPlanetsFromLocal();
+		console.log('i else')
 		$scope.timeToPlay();
 	}
 };
@@ -60,34 +58,34 @@ $scope.fetchPeople = function(){
 	// And save all our API-responses to local storage, to avoid load-time. 
 		StarModel.savePlanetsLocalStorage();
 
-	// Now loading is done. Set load-variable to false and it's time to play! 
+	// Now loading is done.
 		$scope.timeToPlay();
 	})
 };
 
-var t1 = new TimelineMax({delay:2, onComplete:$scope.animateMenu})
-t1.to("#introtext", 1, {'opacity':0, 'scale':0});
-t1.to("#introplanet", 1, {y:-200}, "-=0.25");
-t1.to('#audio1', 2, {'volume':0});
+
 
 // Animations to remove intro-text, loadingmessage and creating menu
 $scope.timeToPlay = function(){
+	TweenMax.to('#loading-message', 1, {opacity:0});
+
+	var t1 = new TimelineMax({delay:2});
+		t1.to("#introtext", 1, {'opacity':0, 'scale':0});
+		t1.to("#introplanet", 1, {y:-220}, "-=0.25");
+		t1.to('#audio1', 2, {'volume':0});
+		t1.play();
 	
-	$scope.menu = true;
-	$scope.loading = false;
-	t1.play();
-	$timeout(function() {
-    	$location.path('/instructions');	
-   }, 5900);
+		$scope.loading = false;
+
 };
 
-var t2 = new TimelineMax();
-	t2.from('#leftMenu', 0.8, {'x':'-100%', 'delay':3});
-	t2.from('#rightMenu', 0.8, {'x':'100%', 'delay':3});
-
 $scope.animateMenu = function(){
-	if($scope.path == '/home'){
-	t2.play();
+	if($scope.path === '/home'){
+	TweenMax.from('#leftMenu', 0.8, {'x':'-100%', 'delay':2})
+	TweenMax.from('#rightMenu', 0.8, {'x':'100%', 'delay':2})
+	$timeout(function() {
+		$location.path('/instructions');	
+  	}, 4500);
 	}
 };
 
